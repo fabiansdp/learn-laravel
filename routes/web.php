808e7b8edec7e85e6aa7cbb5a969b118f91b1525
halo_apping <apping@lokakarya.co.id>
2020-05-28 15:52:12 +0700

    add-routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/playlist', function () {
+    return view('playlist');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
